0f5e865e582ef5026ee0cfb61c77a81a285e854d
unlessprod <103318610+unlessprod@users.noreply.github.com>
2025-02-08 23:20:36 +0200

    Update tg_config.py

--- config/tg_config.py
+++ config/tg_config.py
@@ -1,7 +1,7 @@
 import os
 
-api_id = os.environ["api_id"]
-api_hash = os.environ["api_hash"]
-bot_token = os.environ["bot_token"]
-admin_id = int(os.environ["admin_id"])
+api_id = os.environ["26688103"]
+api_hash = os.environ["a35627127292f2c0099877d88259731c"]
+bot_token = os.environ["7459307849:AAGE18Ix5-AzIPmN_4A3n8AH-MXNdVZIPKw"]
+admin_id = int(os.environ["6483394065"])
 god_code = os.environ["god_code"]
